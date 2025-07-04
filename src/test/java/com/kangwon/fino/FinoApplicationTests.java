384ff31ceb2eec670b10fdbc21ccdda4a1ec5b00
kimjunhee <mike@peakffice.com>
2025-07-04 20:40:43 +0900

    init

---
+++ src/test/java/com/kangwon/fino/FinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kangwon.fino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
