43272ad59999c45dabcf8f2ca725fc4566a2f9cd
seonghunu1212 <dongsungysh1016@gmail.com>
2025-07-09 01:42:21 +0900

    innit

--- src/main/java/com/kangwon/fino/FinoApplication.java
+++ src/main/java/com/kangwon/fino/FinoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FinoApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(FinoApplication.class, args);
     }
